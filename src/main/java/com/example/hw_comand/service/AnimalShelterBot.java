cb7040d114f81671dc5b82c4a03aec407e30fddb
AllaKulchitskaya <alla070@list.ru>
2023-03-06 18:36:50 +0300

    Подключили бота, добавили приветствие пользователя

---
+++ src/main/java/com/example/hw_comand/service/AnimalShelterBot.java
@@ -0,0 +1,64 @@
+package com.example.hw_comand.service;
+
+import com.example.hw_comand.config.AnimalShelterBotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Slf4j
+public class AnimalShelterBot extends TelegramLongPollingBot {
+    final AnimalShelterBotConfig config;
+
+    public AnimalShelterBot(AnimalShelterBotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Неверный формат сообщения");
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Привет, " + name + "!\nДобро пожаловать в наш приют для животных!";
+        log.info("Ответ пользователю " + name);
+
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Произошла ошибка: " + e.getMessage());
+        }
+    }
+}
