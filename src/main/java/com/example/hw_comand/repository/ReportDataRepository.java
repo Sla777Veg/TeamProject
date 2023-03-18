704ee025bb070a5d412996853c48d10c04c2be0f
Damir294 <117120261+Damir294@users.noreply.github.com>
2023-03-18 12:47:25 +0400

    обнова кода от 17.03.2023  Созданы контроллеры ReportDataController,но есть ошибки компиляции

---
+++ src/main/java/com/example/hw_comand/repository/ReportDataRepository.java
@@ -0,0 +1,18 @@
+package com.example.hw_comand.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+
+@Repository
+public interface ReportDataRepository extends JpaRepository<ReportData, Long> {
+
+    Set<ReportData> findListByChatId(Long id);
+
+    ReportData findByChatId(Long id);
+
+}
