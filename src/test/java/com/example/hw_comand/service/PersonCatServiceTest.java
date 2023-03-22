a2da0a54a91006e3340f74739494d8c5a5071c1a
Damir294 <117120261+Damir294@users.noreply.github.com>
2023-03-23 00:33:17 +0400

    23.03.2023 Добавлены тесты по контроллерам и сервису

---
+++ src/test/java/com/example/hw_comand/service/PersonCatServiceTest.java
@@ -0,0 +1,193 @@
+package com.example.hw_comand.service;
+
+import com.example.hw_comand.exceptions.DogNotFoundException;
+import com.example.hw_comand.exceptions.PersonCatNotFoundException;
+import com.example.hw_comand.model.Cat;
+import com.example.hw_comand.model.Dog;
+import com.example.hw_comand.model.PersonCat;
+import com.example.hw_comand.repository.DogRepository;
+import com.example.hw_comand.repository.PersonCatRepository;
+import com.example.hw_comand.service.CatService;
+import com.example.hw_comand.service.DogService;
+import com.example.hw_comand.service.PersonCatService;
+import org.assertj.core.api.Assertions;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Class for testing PersonCatService
+ * @see PersonCatService
+ * @see PersonCatRepository
+ * @author Obukhov Damir
+ */
+@ExtendWith(MockitoExtension.class)
+class PersonCatServiceTest {
+
+    @Mock
+    private PersonCatRepository personCatRepositoryMock;
+
+    @InjectMocks
+    private PersonCatService personCatService;
+
+    /**
+     * Test for method <b>getById()</b> in PersonCatService
+     * <br>
+     * Mockito: when <b>PersonCatRepository::findById()</b> method called, returns <b>expected</b> object
+     */
+    @Test
+    public void getByIdTest() {
+        PersonCat expected = new PersonCat(1L, "testName", 2000, "testPhone", "testMail", "testAddress", 1L);
+
+        Mockito.when(personCatRepositoryMock.findById(any(Long.class))).thenReturn(Optional.of(expected));
+
+        PersonCat actual = personCatService.getById(1L);
+
+        Assertions.assertThat(actual.getId()).isEqualTo(expected.getId());
+        Assertions.assertThat(actual.getName()).isEqualTo(expected.getName());
+        Assertions.assertThat(actual.getYearOfBirth()).isEqualTo(expected.getYearOfBirth());
+        Assertions.assertThat(actual.getPhone()).isEqualTo(expected.getPhone());
+        Assertions.assertThat(actual.getMail()).isEqualTo(expected.getMail());
+        Assertions.assertThat(actual.getAddress()).isEqualTo(expected.getAddress());
+        Assertions.assertThat(actual.getChatId()).isEqualTo(expected.getChatId());
+    }
+
+    /**
+     * Test for throwing an exception in method <b>getById()</b> in PersonCatService
+     * <br>
+     * Mockito: when <b>PersonCatRepository::findById()</b> method called, throws <b>PersonCatNotFoundException</b>
+     */
+    @Test
+    public void getByIdExceptionTest() {
+        Mockito.when(personCatRepositoryMock.findById(any(Long.class))).thenThrow(PersonCatNotFoundException.class);
+
+        org.junit.jupiter.api.Assertions.assertThrows(PersonCatNotFoundException.class, () -> personCatService.getById(1L));
+    }
+
+    /**
+     * Test for method <b>create()</b> in PersonCatService
+     * <br>
+     * Mockito: when <b>PersonCatRepository::save()</b> method called, returns <b>expected</b> object
+     */
+    @Test
+    public void createTest() {
+        PersonCat expected = new PersonCat(1L, "testName", 2000, "testPhone", "testMail", "testAddress", 1L);
+
+        Mockito.when(personCatRepositoryMock.save(any(PersonCat.class))).thenReturn(expected);
+
+        PersonCat actual = personCatService.create(expected);
+
+        Assertions.assertThat(actual.getId()).isEqualTo(expected.getId());
+        Assertions.assertThat(actual.getName()).isEqualTo(expected.getName());
+        Assertions.assertThat(actual.getYearOfBirth()).isEqualTo(expected.getYearOfBirth());
+        Assertions.assertThat(actual.getPhone()).isEqualTo(expected.getPhone());
+        Assertions.assertThat(actual.getMail()).isEqualTo(expected.getMail());
+        Assertions.assertThat(actual.getAddress()).isEqualTo(expected.getAddress());
+        Assertions.assertThat(actual.getChatId()).isEqualTo(expected.getChatId());
+    }
+
+    /**
+     * Test for method <b>update()</b> in PersonCatService
+     * <br>
+     * Mockito: when <b>PersonCatRepository::save()</b> method called, returns <b>expected</b> object
+     */
+    @Test
+    public void updateTest() {
+        PersonCat expected = new PersonCat(1L, "testName", 2000, "testPhone", "testMail", "testAddress", 1L);
+
+        Mockito.when(personCatRepositoryMock.findById(any(Long.class))).thenReturn(Optional.of(expected));
+        Mockito.when(personCatRepositoryMock.save(any(PersonCat.class))).thenReturn(expected);
+
+        PersonCat actual = personCatService.update(expected);
+
+        Assertions.assertThat(actual.getId()).isEqualTo(expected.getId());
+        Assertions.assertThat(actual.getName()).isEqualTo(expected.getName());
+        Assertions.assertThat(actual.getYearOfBirth()).isEqualTo(expected.getYearOfBirth());
+        Assertions.assertThat(actual.getPhone()).isEqualTo(expected.getPhone());
+        Assertions.assertThat(actual.getMail()).isEqualTo(expected.getMail());
+        Assertions.assertThat(actual.getAddress()).isEqualTo(expected.getAddress());
+        Assertions.assertThat(actual.getChatId()).isEqualTo(expected.getChatId());
+    }
+
+    /**
+     * Test for throwing an exception in method <b>update()</b> in PersonCatService
+     * <br>
+     * Creating a <b>PersonCat</b> object with null id
+     */
+    @Test
+    public void updateExceptionTest() {
+        PersonCat expected = new PersonCat();
+
+        org.junit.jupiter.api.Assertions.assertThrows(PersonCatNotFoundException.class, () -> personCatService.update(expected));
+    }
+
+    /**
+     * Test for method <b>getAll()</b> in PersonCatService
+     * <br>
+     * Mockito: when <b>PersonCatRepository::findAll()</b> method called, returns <b>expected</b> collection
+     */
+    @Test
+    public void getAllTest() {
+        List<PersonCat> expected = new ArrayList<>();
+
+        PersonCat testPerson1 = new PersonCat(1L, "testName1", 1995, "testPhone1", "testMail1", "testAddress1", 1L);
+        expected.add(testPerson1);
+
+        PersonCat testPerson2 = new PersonCat(2L, "testName1", 1997, "testPhone2", "testMail2", "testAddress2", 2L);
+        expected.add(testPerson2);
+
+        PersonCat testPerson3 = new PersonCat(3L, "testName3", 2005, "testPhone3", "testMail3", "testAddress3", 3L);
+        expected.add(testPerson3);
+
+        Mockito.when(personCatRepositoryMock.findAll()).thenReturn(expected);
+
+        Collection<PersonCat> actual = personCatService.getAll();
+
+        Assertions.assertThat(actual.size()).isEqualTo(expected.size());
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    /**
+     * Test for method <b>getByChatId()</b> in PersonCatService
+     * <br>
+     * Mockito: when <b>PersonCatRepository::findByChatId()</b> method called, returns <b>expected</b> collection
+     */
+    @Test
+    public void getByChatIdTest() {
+        Set<PersonCat> expected = new HashSet<>();
+
+        PersonCat testPerson1 = new PersonCat(1L, "testName1", 1995, "testPhone1", "testMail1", "testAddress1", 1L);
+        expected.add(testPerson1);
+
+        PersonCat testPerson2 = new PersonCat(2L, "testName1", 1997, "testPhone2", "testMail2", "testAddress2", 2L);
+        expected.add(testPerson2);
+
+        PersonCat testPerson3 = new PersonCat(3L, "testName3", 2005, "testPhone3", "testMail3", "testAddress3", 3L);
+        expected.add(testPerson3);
+
+        Mockito.when(personCatRepositoryMock.findByChatId(any(Long.class))).thenReturn(expected);
+
+        Collection<PersonCat> actual = personCatService.getByChatId(1L);
+
+        Assertions.assertThat(actual.size()).isEqualTo(expected.size());
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
--- src/test/java/com/example/hw_comand/service/ReportDataServiceTest.java
+++ src/test/java/com/example/hw_comand/service/ReportDataServiceTest.java
@@ -1,7 +1,197 @@
 package com.example.hw_comand.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.hw_comand.exceptions.DogNotFoundException;
+import com.example.hw_comand.exceptions.PersonCatNotFoundException;
+import com.example.hw_comand.exceptions.PersonDogNotFoundException;
+import com.example.hw_comand.exceptions.ReportDataNotFoundException;
+import com.example.hw_comand.model.*;
+import com.example.hw_comand.repository.DogRepository;
+import com.example.hw_comand.repository.PersonCatRepository;
+import com.example.hw_comand.repository.PersonDogRepository;
+import com.example.hw_comand.repository.ReportDataRepository;
+import com.example.hw_comand.service.CatService;
+import com.example.hw_comand.service.DogService;
+import com.example.hw_comand.service.PersonCatService;
+import org.assertj.core.api.Assertions;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-class ReportDataServiceTest {
+import java.util.*;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+/**
+ * Class for testing ReportDataService
+ * @see ReportDataService
+ * @see ReportDataRepository
+ * @author Obukhov Damir
+ */
+@ExtendWith(MockitoExtension.class)
+public class ReportDataServiceTest {
+
+    @Mock
+    private ReportDataRepository reportDataRepositoryMock;
+
+    @InjectMocks
+    private ReportDataService reportDataService;
+
+    /**
+     * Test for method <b>findById()</b> in ReportDataService
+     * <br>
+     * Mockito: when <b>ReportDataRepository::findById</b> method called, returns <b>expected</b> object
+     */
+    @Test
+    public void findByIdTest() {
+        ReportData expected = new ReportData();
+        expected.setChatId(1L);
+        expected.setRation("testRation");
+        expected.setDays(1L);
+        expected.setHabits("testHabits");
+        expected.setCaption("testCaption");
+
+        Mockito.when(reportDataRepositoryMock.findById(any(Long.class))).thenReturn(Optional.of(expected));
+
+        ReportData actual = reportDataService.findById(1L);
+
+        Assertions.assertThat(actual.getChatId()).isEqualTo(expected.getChatId());
+        Assertions.assertThat(actual.getRation()).isEqualTo(expected.getRation());
+        Assertions.assertThat(actual.getDays()).isEqualTo(expected.getDays());
+        Assertions.assertThat(actual.getHabits()).isEqualTo(expected.getHabits());
+        Assertions.assertThat(actual.getCaption()).isEqualTo(expected.getCaption());
+    }
+
+    /**
+     * Test for throwing an exception in method <b>findById()</b> in ReportDataService
+     * <br>
+     * Mockito: when <b>ReportDataRepository::findById()</b> method called, throws <b>ReportDataNotFoundException</b>
+     */
+    @Test
+    public void findByIdExceptionTest() {
+        Mockito.when(reportDataRepositoryMock.findById(any(Long.class))).thenThrow(ReportDataNotFoundException.class);
+
+        org.junit.jupiter.api.Assertions.assertThrows(ReportDataNotFoundException.class, () -> reportDataService.findById(1L));
+    }
+
+    /**
+     * Test for method <b>findByChatId()</b> in ReportDataService
+     * <br>
+     * Mockito: when <b>ReportDataRepository::findByChatId()</b> method called, returns <b>expected</b> object
+     */
+    @Test
+    public void findByChatIdTest() {
+        ReportData expected = new ReportData();
+        expected.setChatId(1L);
+        expected.setRation("testRation");
+        expected.setDays(1L);
+        expected.setHabits("testHabits");
+        expected.setCaption("testCaption");
+
+        Mockito.when(reportDataRepositoryMock.findByChatId(any(Long.class))).thenReturn(expected);
+
+        ReportData actual = reportDataService.findByChatId(1L);
+
+        Assertions.assertThat(actual.getChatId()).isEqualTo(expected.getChatId());
+        Assertions.assertThat(actual.getRation()).isEqualTo(expected.getRation());
+        Assertions.assertThat(actual.getDays()).isEqualTo(expected.getDays());
+        Assertions.assertThat(actual.getHabits()).isEqualTo(expected.getHabits());
+        Assertions.assertThat(actual.getCaption()).isEqualTo(expected.getCaption());
+    }
+
+    /**
+     * Test for method <b>findListByChatId()</b> in ReportDataService
+     * <br>
+     * Mockito: when <b>ReportDataRepository::findListByChatId()</b> method called, returns <b>expected</b> collection
+     */
+    @Test
+    public void findListByChatIdTest() {
+        Set<ReportData> expected = new HashSet<>();
+
+        ReportData testReport1 = new ReportData();
+        testReport1.setChatId(1L);
+        testReport1.setRation("testRation1");
+        testReport1.setDays(1L);
+        testReport1.setHabits("testHabits1");
+        testReport1.setCaption("testCaption1");
+        expected.add(testReport1);
+
+        ReportData testReport2 = new ReportData();
+        testReport1.setChatId(1L);
+        testReport1.setRation("testRation2");
+        testReport1.setDays(2L);
+        testReport1.setHabits("testHabits2");
+        testReport1.setCaption("testCaption2");
+        expected.add(testReport2);
+
+        ReportData testReport3 = new ReportData();
+        testReport1.setChatId(1L);
+        testReport1.setRation("testRation3");
+        testReport1.setDays(3L);
+        testReport1.setHabits("testHabits3");
+        testReport1.setCaption("testCaption3");
+        expected.add(testReport3);
+
+        Mockito.when(reportDataRepositoryMock.findListByChatId(any(Long.class))).thenReturn(expected);
+
+        Collection<ReportData> actual = reportDataService.findListByChatId(1L);
+
+        Assertions.assertThat(actual.size()).isEqualTo(expected.size());
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    /**
+     * Test for method <b>getAll()</b> in ReportDataService
+     * <br>
+     * Mockito: when <b>ReportDataRepository::findAll()</b> method called, returns <b>expected</b> collection
+     */
+    @Test
+    public void findAllTest() {
+        List<ReportData> expected = new ArrayList<>();
+
+        ReportData testReport1 = new ReportData();
+        testReport1.setChatId(1L);
+        testReport1.setRation("testRation1");
+        testReport1.setDays(1L);
+        testReport1.setHabits("testHabits1");
+        testReport1.setCaption("testCaption1");
+        expected.add(testReport1);
+
+        ReportData testReport2 = new ReportData();
+        testReport1.setChatId(1L);
+        testReport1.setRation("testRation2");
+        testReport1.setDays(2L);
+        testReport1.setHabits("testHabits2");
+        testReport1.setCaption("testCaption2");
+        expected.add(testReport2);
+
+        ReportData testReport3 = new ReportData();
+        testReport1.setChatId(1L);
+        testReport1.setRation("testRation3");
+        testReport1.setDays(3L);
+        testReport1.setHabits("testHabits3");
+        testReport1.setCaption("testCaption3");
+        expected.add(testReport3);
+
+        Mockito.when(reportDataRepositoryMock.findAll()).thenReturn(expected);
+
+        Collection<ReportData> actual = reportDataService.getAll();
+
+        Assertions.assertThat(actual.size()).isEqualTo(expected.size());
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
 }
